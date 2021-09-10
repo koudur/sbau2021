af2392c401e30183ac707b08fe7c6fd963c2e85a
LeMinaw <leminaw@gmail.com>
2021-09-10 15:31:25 +0200

    more fonts formats

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Gotham', ...defaultTheme.fontFamily.sans],
+                sans: ['gotham', ...defaultTheme.fontFamily.sans],
             },
         },
     },
